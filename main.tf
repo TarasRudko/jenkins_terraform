6b1b50f92a0ee8d0807d129f0d04844f9572f3d2
TarasRudko <47573756+TarasRudko@users.noreply.github.com>
2021-03-16 13:16:23 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,3 +4,21 @@ terraform {
     credentials = "./creds/serviceaccount.json"
   }
 }
+
+terraform {
+  required_version = ">= 0.14"
+  required_providers {
+    google      = "~> 3.55.0"
+    google-beta = "~> 3.55.0"
+  }
+}
+
+provider "google" {
+  region  = var.region
+  project = var.project
+}
+
+provider "google-beta" {
+  region  = var.region
+  project = var.project
+}
