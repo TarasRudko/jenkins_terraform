ed5817db50a3eda554d610e51adf1797c98c65d3
Taras Rudko <taras.rudko@gmail.com>
2021-03-17 12:53:17 +0200

    Fix dir

---
+++ infrastructure/pheonix/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  backend "gcs" {
+    bucket = "ruta2-303211-tfstate-abc"
+  }
+}
+
+terraform {
+  required_version = ">= 0.14"
+  required_providers {
+    google      = "~> 3.55.0"
+    google-beta = "~> 3.55.0"
+  }
+}
+
+provider "google" {
+  region  = var.region
+  project = var.project
+}
+
+provider "google-beta" {
+  region  = var.region
+  project = var.project
+}
